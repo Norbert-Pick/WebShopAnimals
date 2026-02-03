82da9b3b9db18f7f2a258de177d5363c5ba442a3
Pick, Norbert <norbert.pick@datev.de>
2026-02-03 12:10:18 +0100

    Tailwind installiert

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../../models/product';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, CommonModule, CurrencyPipe],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
