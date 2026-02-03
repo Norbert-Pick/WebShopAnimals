9a9b5ebc322bacaba199c2f129af867f1898731a
Pick, Norbert <norbert.pick@datev.de>
2026-02-03 18:49:56 +0100

    Warenkorb (Teil 2)

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { Product } from '../../models/product';
@@ -13,6 +14,7 @@ import { CartService } from '../cart.service';
     CommonModule,
     MatListModule,
     MatIconModule,
+    MatCardModule,
     MatButtonModule
   ],
   templateUrl: './cart-view.component.html',
