82e8b417b4194b38604d4d2b34a852db25637a53
Pick, Norbert <norbert.pick@datev.de>
2026-02-03 18:29:49 +0100

    Cart hinzugefügt (1)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { CartViewComponent } from './cart/cart-view/cart-view.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
-
 
 export const routes: Routes = [
     {path: '', redirectTo: 'products', pathMatch: 'full'},
-    {path: 'products', component: ProductListComponent}
+    {path: 'products', component: ProductListComponent},
+    {path: 'cart', component: CartViewComponent}
     
 ];
---
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { Product } from '../../models/product';
+import { CartService } from '../cart.service';
+
+
+@Component({
+  selector: 'app-cart-view',
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './cart-view.component.html',
+  styleUrl: './cart-view.component.css',
+})
+export class CartViewComponent implements OnInit {
+  cartItems : Product[] = [];
+
+  constructor(private cartService: CartService) {
+   
+  }
+
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe((data) => {
+      this.cartItems = data;
+    });
+  }
+
+
+}
