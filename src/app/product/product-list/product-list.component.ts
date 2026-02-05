676d7103d85672537f1a1ecccdcc32b16c301a39
Pick, Norbert <norbert.pick@datev.de>
2026-02-05 12:02:22 +0100

    Zweiter IntelliJ Commit

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { CartService } from 'src/app/cart/cart.service';
 import { Product } from '../../models/product';
@@ -15,6 +16,7 @@ import { ProductService } from '../product.service';
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
+    MatInputModule,
     CommonModule,
     CurrencyPipe],
   templateUrl: './product-list.component.html',
@@ -22,14 +24,13 @@ import { ProductService } from '../product.service';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
 
   constructor(
     private productService: ProductService,
     private cartService: CartService,
     private snackbar: MatSnackBar,
   ) {
-
-
   }
 
   ngOnInit(): void {
@@ -46,8 +47,23 @@ export class ProductListComponent implements OnInit {
           duration: 3000,
           verticalPosition: 'top',
           horizontalPosition: 'right',
+          panelClass: ['app-snackbar-top-left', 'app-snackbar-text-14']
         },);
       }
     });
   };
+
+  applyFilter(event: Event): void {
+    let searchTerm = (event.target as HTMLInputElement).value;
+    searchTerm = searchTerm.toLowerCase();
+    this this.filteredProducts = this.products.filter(
+        (product: product.n) => {})
+
+      const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+    this.productService.getProducts().subscribe((geholteProduktliste) => {
+      this.products = geholteProduktliste.filter(product =>
+        product.name.toLowerCase().includes(filterValue) ||
+        product.description.toLowerCase().includes(filterValue)
+      );
+    });
 }
